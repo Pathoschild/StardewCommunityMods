bd69144a0e1f6eb0b10517d26bf7d3bdf4757a85
alexb5dh <alexb5dh@gmail.com>
2016-10-19 11:42:31 +0300

    version update

--- TimeSpeed/AssemblyInfo.cs
+++ TimeSpeed/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d617dc5d-0495-4aa3-98ce-8c80baf66c33")]
-[assembly: AssemblyFileVersion("1.9.1.0")]
-[assembly: AssemblyVersion("1.9.1.0")]
+[assembly: AssemblyFileVersion("1.9.2.0")]
+[assembly: AssemblyVersion("1.9.2.0")]
 [assembly: TargetFramework(".NETFramework,Version=v4.5", FrameworkDisplayName=".NET Framework 4.5")]
 
