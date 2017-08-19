c75bbc547338111971017b15eb05477d24ac786d
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-08-18 23:34:37 -0400

    remove TractorMod (adopted into my own mods)

--- TractorMod/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TractorMod")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("0510e2ac-ec06-4211-b733-faf9d3a069ea")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
